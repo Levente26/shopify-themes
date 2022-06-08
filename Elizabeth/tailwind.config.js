95cc6b3efb349eb418228c72dd7a39d4aec4bbc6
Levente26 <madi.levente26@gmail.com>
2022-06-08 13:27:35 +0200

    feat: navigation bar

--- Elizabeth/tailwind.config.js
+++ Elizabeth/tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  content: [],
+  content: [
+    "./assets/*.liquid",
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/*.liquid",
+    "./templates/**/*.liquid",
+  ],
   theme: {
     extend: {},
   },
